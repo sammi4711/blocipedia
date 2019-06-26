8a9df43adda47ae2779c1ca4ba53714b22b1ca54
Samantha Acampora <samantha.acampora@gmail.com>
2019-06-26 00:37:16 -0400

    Still trying

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -65,7 +65,8 @@ Rails.application.configure do
 
   config.action_mailer.perform_deliveries = true
   config.action_mailer.raise_delivery_errors = true
-  config.action_mailer.default_url_options = { host: ENV['MAIL_HOST'] }
+  #config.action_mailer.default_url_options = { host: ENV['MAIL_HOST'] }
+  config.action_mailer.default_url_options = { host: 'mail.google.com', port: 587}
   config.action_mailer.delivery_method = :smtp
   config.action_mailer.smtp_settings = {
     user_name:      ENV['SENDMAIL_USERNAME'],
@@ -76,4 +77,6 @@ Rails.application.configure do
     authentication: :plain,
     enable_starttls_auto: true
   }
+  #config.action_mailer.sender = 'samantha.acampora@gmail.com'
+  config.mailer_sender = 'samantha.acampora@gmail.com'
 end
