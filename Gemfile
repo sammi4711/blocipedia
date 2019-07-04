2617eeecc25c77e129ed7a0999114ff808af080f
Samantha Acampora <samantha.acampora@gmail.com>
2019-07-04 19:22:53 -0400

    Markdown implemented

--- Gemfile
+++ Gemfile
@@ -84,3 +84,5 @@ gem 'faker'
 gem 'stripe'
 
 gem 'figaro', '1.0'
+
+gem 'redcarpet'
--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,18 @@
+require 'redcarpet'
+
 module ApplicationHelper
+  def markdown(text)
+    options = {
+      hard_wrap: true,
+      filter_html: true
+    }
+    extensions = { 
+      autolink: true, 
+      no_intraemphasis: true
+    }
+    renderer = Redcarpet::Render::HTML.new(options)
+    markdown = Redcarpet::Markdown.new(renderer, extensions)
+
+    markdown.render(text).html_safe
+  end
 end
